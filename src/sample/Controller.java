4a4fa7347925c433e9ad4972bb70705b6ba9b844
Michael Hochleitner <michaelhochleitner17@gmail.com>
2018-05-13 11:50:16 +0200

    directed movement, treasure count, step count

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -20,14 +20,59 @@ public class Controller implements Initializable{
     private StringProperty yPos = new SimpleStringProperty("5");
     private StringProperty xPos = new SimpleStringProperty("5");
 
+    @FXML
+    private StringProperty poiFound = new SimpleStringProperty("0");
+    @FXML
+    private StringProperty stepsNeeded = new SimpleStringProperty("0");
+
     private Set placesOfInterest;
 
     @FXML private Label yLabel;
     @FXML private Label xLabel;
     @FXML private Label poiLabel;
+    @FXML private Label poifound;
+    @FXML private Label steps;
 
-    @FXML protected void handleActionButtonAction(ActionEvent event) {
+    @FXML protected void handleActionButtonAction() {
         move();
+    }
+
+    @FXML protected void handleUpButton(){
+        incrementPosition(yPos);
+        incrementStringProperty(stepsNeeded);
+    }
+
+    @FXML protected void handleRightButton(){
+        incrementPosition(xPos);
+        incrementStringProperty(stepsNeeded);
+    }
+
+    @FXML protected void handleLeftButton() {
+        decrementPosition(xPos);
+        incrementStringProperty(stepsNeeded);
+    }
+
+    @FXML protected void handleDownButton() {
+        decrementPosition(yPos);
+        incrementStringProperty(stepsNeeded);
+    }
+
+    private void decrementPosition(StringProperty stringProperty) {
+        int posInt = intFrom(stringProperty);
+        if (posInt > 0){
+            posInt -= 1;
+        }
+        setStringPropertyValueWith(stringProperty, posInt);
+        checkForPlaceOfInterest();
+    }
+
+    private void incrementPosition(StringProperty stringProperty) {
+        int posInt = intFrom(stringProperty);
+        if (posInt < 9){
+            posInt += 1;
+        }
+        setStringPropertyValueWith(stringProperty, posInt);
+        checkForPlaceOfInterest();
     }
 
     private boolean randomBoolean(){
@@ -44,9 +89,9 @@ public class Controller implements Initializable{
 
     private void move(){
         if (randomBoolean()){
-            incrementPos(xPos);
+            randomPositionChange(xPos);
         } else {
-            incrementPos(yPos);
+            randomPositionChange(yPos);
         }
         checkForPlaceOfInterest();
     }
@@ -56,35 +101,55 @@ public class Controller implements Initializable{
         int yPosInt = Integer.parseInt(yPos.getValue());
         Tuple currentPos = new Tuple(xPosInt, yPosInt);
 
+        Object foundTuple = null;
+
         boolean poifound = false;
         for (Object tuplex : placesOfInterest) {
             if(tuplex.equals(currentPos)){
                 poifound = true;
+                foundTuple = tuplex;
             }
         }
 
         if (poifound) {
-            poiLabel.textProperty().setValue("You found a place of interest");
+            incrementStringProperty(poiFound);
+            poiLabel.textProperty().setValue("You found a treasure!");
+            placesOfInterest.remove(foundTuple);
         } else {
             poiLabel.textProperty().setValue("common tile");
         }
     }
 
-    private void incrementPos(StringProperty stringProperty) {
-        String posString = stringProperty.getValue();
-        int posInt = Integer.parseInt(posString);
+    private void incrementStringProperty(StringProperty stringProperty) {
+        int poif = intFrom(stringProperty);
+        poif +=1;
+        setStringPropertyValueWith(stringProperty,poif);
+    }
+
+    private void randomPositionChange(StringProperty stringProperty) {
+        int posInt = intFrom(stringProperty);
         switch (posInt) {
-            case 10: posInt -= 1;
+            case 9: posInt -= 1;
             break;
 
-            case 1: posInt += 1;
+            case 0: posInt += 1;
             break;
 
             default: posInt += plusOrMinusOne();
             break;
         }
+        setStringPropertyValueWith(stringProperty, posInt);
+    }
+
+    private void setStringPropertyValueWith(StringProperty stringProperty, int posInt) {
+        String posString;
         posString = Integer.toString(posInt);
         stringProperty.setValue(posString);
+    }
+
+    private int intFrom(StringProperty stringProperty) {
+        String posString = stringProperty.getValue();
+        return Integer.parseInt(posString);
     }
 
     @Override
@@ -102,6 +167,8 @@ public class Controller implements Initializable{
         placesOfInterest.add(pointOfInterest());
         yLabel.textProperty().bind(yPos);
         xLabel.textProperty().bind(xPos);
+        poifound.textProperty().bind(poiFound);
+        steps.textProperty().bind(stepsNeeded);
     }
 
     private Tuple pointOfInterest() {
